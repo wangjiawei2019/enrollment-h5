1bf972b9c28baa43082de108b33afec783912589
IdlerHub <18459406114@163.com>
2020-05-18 17:55:51 +0800

    引入插件&环境配置&登录页

--- src/router/index.js
+++ src/router/index.js
@@ -7,16 +7,28 @@
 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Home from '../views/Home'
+import Index from '../views/Index'
+import Login from '../views/Login'
 
 Vue.use(VueRouter)
 
 const routes = [
+  { path: '/', redirect: 'login' },
   {
-    path: '/',
-    name: 'Home',
-    component: Home,
+    path: '/login',
+    name: 'Login',
+    component: Login,
   },
+  {
+    path: '/index',
+    name: 'Index',
+    component: Index,
+    children:[
+      { path: '/', redirect: '/index/home' },
+      { path: '/index/home', component: Home}
+    ]
+  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/Index.vue
@@ -0,0 +1,37 @@
+<!--
+ * @Author: zxk
+ * @Date: 2020-05-18 14:01:48
+ * @LastEditors: zxk
+ * @LastEditTime: 2020-05-18 17:54:10
+--> 
+<template>
+  <div id="index">
+    index
+    <router-view />
+    <van-tabbar v-model="active">
+        <van-tabbar-item icon="home-o">标签</van-tabbar-item>
+        <van-tabbar-item icon="search">标签</van-tabbar-item>
+        <van-tabbar-item icon="friends-o">标签</van-tabbar-item>
+        <van-tabbar-item icon="setting-o">标签</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script>
+import { Tabbar, TabbarItem } from 'vant';
+export default {
+  name: 'Index',
+  data() {
+    return {
+      active: 0,
+    };
+  },
+  components:{
+    'van-tabbar':Tabbar,
+    'van-tabbar-item':TabbarItem
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
